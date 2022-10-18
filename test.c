66fec6b9bfb994d27ff79689d589a44eb743e6c0
Azeddine El makkaoui <ael-makk@e1r6p5.1337.ma>
2022-10-18 19:05:33 +0100

    backup

--- test.c
+++ test.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
 
 int main(int argc, char *argv[])
 {
 	size_t spn = 0;
-	size_t cspn = 0;
+	size_t date;
 
-	if (argc > 1) {
-		cspn = strcspn("PwBLgNa8p8MTKW57S7zxVAQCxnCpV8JqTTs9XEBv\n", "\n");
-		spn = strspn("PwBLgNa8p8MTKW57S7zxVAQCxnCpV8JqTTs9XEBv\n", "\n");
-		printf ("strcspn : %lu\n", cspn);
-		printf("strspn : %lu\n", spn);
-	}
+		date = (size_t)time(0);
+		printf ("time : %lu\n", date);
+	
 	return (0);
 }
